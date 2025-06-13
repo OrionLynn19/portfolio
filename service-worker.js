7807a9eec3ee461692cec48ed3a1c8889d2ac1e0
Lynn Pyae Hmue <orionlynn@Lynns-MacBook-Air-5.local>
2025-06-13 16:34:29 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/OrionLynn19/portfolio/precache-manifest.6908af22075127d4ad4073f2f5844689.js"
+  "./precache-manifest.7d9490fe6c4acb6c373a6cbee073e344.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OrionLynn19/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
